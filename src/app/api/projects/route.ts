17af04f5553f7dc9579c753db73dc9c22f9e67a5
Grecco, Franco <srgrecco93@gmail.com>
2025-07-24 21:44:40 +0200

    feat (api): add projects endpoing

---
+++ src/app/api/projects/route.ts
@@ -0,0 +1,252 @@
+const projects = [
+  {
+    title: "E-commerce product page",
+    description:
+      "In this challenge, you'll build a beautiful product page. We'll be putting your JS skills to the test with a lightbox product gallery and cart functionality!",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "3",
+      label: "intermediate",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-intermediate-01.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/ecommerce-product-page-UPsZ9MJp6",
+      deploy: "https://frontend-mentor-intermediate-01.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-intermediate-01",
+    },
+  },
+  {
+    title: "Time tracking dashboard",
+    description:
+      "A perfect opportunity to practice your CSS Grid skills. For anyone wanting to take it up a notch, we provide a JSON data file to practice working with data.",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-01.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/time-tracking-dashboard-UIQ7167Jw",
+      deploy: "https://frontend-mentor-junior-01.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-01",
+    },
+  },
+  {
+    title: "Testimonials grid section",
+    description:
+      "This challenge will be perfect practice for anyone wanting to test their CSS Grid skills. Grid is such a powerful addition to CSS, so it's worth getting to grips with it!",
+    technologies: ["HTML", "CSS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-02.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/testimonials-grid-section-Nnw6J7Un7",
+      deploy: "https://frontend-mentor-junior-02.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-02",
+    },
+  },
+  {
+    title: "Tip calculator app",
+    description:
+      "This small app is perfect for anyone starting to get to grips with JavaScript. The calculator functionality will be a nice test!",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-03.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX",
+      deploy: "https://frontend-mentor-junior-03.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-03",
+    },
+  },
+  {
+    title: "News homepage",
+    description:
+      "This news homepage will be an excellent opportunity to practice your CSS Grid skills. There will be lots of tricky decisions to make and plenty of learning opportunities!",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-04.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/news-homepage-H6SWTa1MFl",
+      deploy: "https://frontend-mentor-junior-04.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-04",
+    },
+  },
+  {
+    title: "Newsletter sign-up form with success message",
+    description:
+      "This will test your skills with basic form structure, validation, and submission. The success state will also be an excellent opportunity to work with DOM manipulation.",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-05.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/newsletter-signup-form-with-success-message-3FC1AZbNrv",
+      deploy: "https://frontend-mentor-junior-05.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-05",
+    },
+  },
+  {
+    title: "Contact form",
+    description:
+      "Building accessible forms is a crucial task for front-end developers. This challenge will help you practice building a form with several input types and validation.",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "2",
+      label: "junior",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-junior-06.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/contact-form--G-hYlqKJj",
+      deploy: "https://frontend-mentor-junior-06.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-junior-06",
+    },
+  },
+  {
+    title: "Interactive rating component",
+    description:
+      "This is a nice, small project to practice handling user interactions and updating the DOM. Perfect for anyone who has learned the basics of JavaScript!",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-01.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/interactive-rating-component-koxpeBUmI",
+      deploy: "https://frontend-mentor-newbie-01.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-01",
+    },
+  },
+  {
+    title: "Product preview card component",
+    description:
+      "This HTML & CSS-only challenge will be perfect for anyone starting to build responsive projects.",
+    technologies: ["HTML", "CSS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-02.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/product-preview-card-component-GO7UmttRfa",
+      deploy: "https://frontend-mentor-newbie-02.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-02",
+    },
+  },
+  {
+    title: "Four card feature section",
+    description:
+      "A nice layout-based challenge for beginners. This will test anyone who is new to multi-column and responsive layouts.",
+    technologies: ["HTML", "CSS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-03.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/four-card-feature-section-weK1eFYK",
+      deploy: "https://frontend-mentor-newbie-03.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-03",
+    },
+  },
+  {
+    title: "NFT preview card component",
+    description:
+      "This HTML & CSS only challenge is perfect for anyone just starting out or anyone wanting a small project to play around with.",
+    technologies: ["HTML", "CSS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-04.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/nft-preview-card-component-SbdUL_w0U",
+      deploy: "https://frontend-mentor-newbie-04.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-04",
+    },
+  },
+  {
+    title: "Article preview component",
+    description:
+      "Practice your layout skills with this article preview component. There's lots of fun to be had playing around with animations for the sharing icons as well.",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-05.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/article-preview-component-dYBN_pYFT",
+      deploy: "https://frontend-mentor-newbie-05.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-05",
+    },
+  },
+  {
+    title: "FAQ accordion",
+    description:
+      "In this challenge, you'll build an FAQ accordion. This is an extremely common front-end pattern, so it's an excellent opportunity to get some practice in!",
+    technologies: ["HTML", "CSS", "JS"],
+    difficulty: {
+      level: "1",
+      label: "newbie",
+    },
+    urls: {
+      preview: "/images/frontend-mentor-newbie-06.webp",
+      challenge:
+        "https://www.frontendmentor.io/challenges/faq-accordion-wyfFdeBwBz",
+      deploy: "https://frontend-mentor-newbie-06.vercel.app/",
+      repository:
+        "https://github.com/GreccoOliva-Franco/frontend-mentor-newbie-06",
+    },
+  },
+];
+
+export type Project = typeof projects extends Array<infer T>
+  ? T & { id: number }
+  : unknown;
+
+export async function GET() {
+  return Response.json(
+    {
+      sideProjects: projects
+        .slice(0, 2)
+        .map((i, index) => ({ ...i, id: index + 1 })),
+      training: projects.map((i, index) => ({ ...i, id: index + 3 })),
+    },
+    { status: 200 }
+  );
+}
