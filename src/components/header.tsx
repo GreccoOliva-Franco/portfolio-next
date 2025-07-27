0102118890681de6755564da15b301fa4fe08b56
Grecco, Franco <srgrecco93@gmail.com>
2025-07-27 19:59:51 +0200

    feat: add mobile navigation and separate into smaller functions

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,9 +9,18 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from "./ui/navigation-menu";
+} from "@/components/ui/navigation-menu";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
+import { Menu } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const navLinks = [
   {
@@ -33,35 +42,92 @@ const navLinks = [
 ];
 
 export default function Header() {
-  const pathname = usePathname();
-
   return (
-    <header className="flex h-fit w-full items-center justify-between  border-x-2 border-b-2 rounded-b-2xl">
-      <Link href={Page.HOME} className="p-4 flex gap-2 items-center">
+    <header className="flex h-fit w-full items-center justify-between border-x-2 border-b-2 rounded-b-2xl px-4 py-2">
+      <Link href={Page.HOME} className="flex gap-2 items-center">
         <LogoText />
       </Link>
 
-      <NavigationMenu className="mr-4">
-        <NavigationMenuList className="gap-4">
-          {navLinks.map((link) => (
-            <NavigationMenuItem key={link.href}>
-              <NavigationMenuLink asChild>
-                <Link
-                  href={link.href}
-                  data-active={pathname === link.href}
-                  className={cn(
-                    "font-bold",
-                    "data-[active=true]:border-b-3 data-[active=true]:border-green-700"
-                  )}
-                  prefetch={false}
-                >
-                  {link.text.toUpperCase()}
-                </Link>
-              </NavigationMenuLink>
-            </NavigationMenuItem>
-          ))}
-        </NavigationMenuList>
-      </NavigationMenu>
+      <NavigationBar />
     </header>
+  );
+}
+
+function NavigationBar() {
+  const pathname = usePathname();
+
+  return (
+    <>
+      <DesktopNavigation pathname={pathname} />
+      <MobileNavigation pathname={pathname} />
+    </>
+  );
+}
+
+function DesktopNavigation({ pathname }: { pathname: string }) {
+  return (
+    <NavigationMenu className="hidden md:block mr-4">
+      <NavigationMenuList className="gap-4">
+        {navLinks.map((link) => (
+          <NavigationMenuItem key={link.href}>
+            <NavigationMenuLink asChild>
+              <Link
+                href={link.href}
+                data-active={pathname === link.href}
+                className={cn(
+                  "font-bold text-sm",
+                  "data-[active=true]:border-b-2 data-[active=true]:border-primary"
+                )}
+                prefetch={false}
+              >
+                {link.text.toUpperCase()}
+              </Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        ))}
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
+
+function MobileNavigation({ pathname }: { pathname: string }) {
+  const [open, setOpen] = React.useState(false);
+
+  const closeSheet = React.useCallback(() => setOpen(false), [setOpen]);
+
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetTrigger asChild className="md:hidden">
+        <Button variant="ghost" size="icon">
+          <Menu className="h-5 w-5" />
+        </Button>
+      </SheetTrigger>
+      <SheetContent
+        side="top"
+        className="pt-20 px-6 bg-background/95 backdrop-blur-sm"
+      >
+        <SheetTitle className="sr-only">Navigatino Menu</SheetTitle>
+        <nav className="flex flex-col gap-8">
+          {navLinks.map((link) => (
+            <Link
+              key={link.href}
+              href={link.href}
+              onClick={closeSheet}
+              className={cn(
+                "text-2xl font-bold py-3 transition-colors",
+                "hover:text-primary focus:text-primary",
+                pathname === link.href
+                  ? "text-primary border-l-4 border-emerald-600 pl-4"
+                  : "text-foreground pl-6"
+              )}
+              prefetch={false}
+            >
+              {link.text}
+            </Link>
+          ))}
+        </nav>
+        <SheetFooter></SheetFooter>
+      </SheetContent>
+    </Sheet>
   );
 }
