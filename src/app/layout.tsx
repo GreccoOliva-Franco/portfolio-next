9bd892c82ad3aed5f3e7a1ce93bb38b524fb3b52
Grecco, Franco <srgrecco93@gmail.com>
2025-12-07 18:30:25 +0100

    feat: add Vercel analytics and speed insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { cn } from "@/lib/utils";
+
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,9 +21,9 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Grecco.Dev | Full Stack solutions",
-  description: `Grecco.Dev combines honor, ethics, and results in every project.
+  description: `Grecco.Dev achieves client results in every project.
     Software focused on readability, maintainability and performance
-    Specializing in JavaScript, React, Node.js, and PHP.`
+    Specializing in JavaScript, React, Node.js, and PHP.`,
 };
 
 export default function RootLayout({
@@ -38,9 +42,11 @@ export default function RootLayout({
         <div className="flex flex-col max-w-7xl min-h-screen mx-auto">
           <Header />
 
-          <main className="flex-grow w-full">{children}</main>
+          <main className="grow w-full">{children}</main>
 
           <Footer />
+          <Analytics />
+          <SpeedInsights />
         </div>
       </body>
     </html>
