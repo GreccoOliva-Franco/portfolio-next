680b727f0d8d66cf1ac6bb795b3f520e0785531a
Grecco, Franco <srgrecco93@gmail.com>
2025-12-07 17:59:47 +0100

    refactor(landing footer): use internal lib/date

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,9 +1,12 @@
-import dayjs from "dayjs";
+import { date } from "@/lib/date";
+
+export const FOOTER_HEIGH = 64; // = to tw h-16
 
 export default function Footer() {
-  const year = dayjs().year();
+  const year = date().year();
+
   return (
-    <footer className="h-fit w-full border-x-2 border-t-2 rounded-t-2xl">
+    <footer className="h-16 bg-background w-full border-x-2 border-t-2 rounded-t-2xl">
       <p className="text-center font-light p-4">&copy; Grecco.dev - {year}</p>
     </footer>
   );
